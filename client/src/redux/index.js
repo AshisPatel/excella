964e2b2f1549b39e87658751b11789209fb00959
AshisPatel <ashis.n.patel@gmail.com>
2021-11-18 15:19:33 -0600

    Add redux with globalstate for currentPage

---
+++ client/src/redux/index.js
@@ -0,0 +1,14 @@
+import { createStore } from "redux";
+// import reducers to include in root reducer
+import currentPageReducer from "./currentPage";
+const rootReducer = combineReducers({
+    currentPage: currentPageReducer
+});
+
+
+const store = createStore(rootReducer);
+// console log message on every state update
+store.subscribe(() => {
+    console.log(store.getState());
+});
+export default store; 
