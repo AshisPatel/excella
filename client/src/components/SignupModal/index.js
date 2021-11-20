585d5300c29d0486db3ce2e834bc46bed74f8e96
AP <ashis.n.patel@gmail.com>
2021-11-19 19:09:28 -0600

    Add icons and style signup modal

--- client/src/components/SignupModal/index.js
+++ client/src/components/SignupModal/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SignupModal = ({ setShowSignup }) => {
     const [showPassword, setShowPassword] = useState(false);
@@ -14,34 +15,54 @@ const SignupModal = ({ setShowSignup }) => {
         <>
             <div className="modal-wrapper">
                 <form className='sl-form'>
-                    <input
-                        aria-aria-label='username'
-                        name="username"
-                        type="text"
-                        value={formData.username}
-                        placeholder="Username"
-                    />
-                    <input
-                        aria-label="email"
-                        name="email"
-                        type="email"
-                        value={formData.email}
-                        placeholder="yourock@email.com"
-                    />
-                    <input
-                        aria-label='password'
-                        name="password"
-                        type={showPassword ? "text" : "password"}
-                        value={formData.password}
-                        placeholder="Password"
-                    />
-                    <input
-                        aria-label='confirm password'
-                        name="confirmPassword"
-                        type={showPassword ? "text" : "password"}
-                        value={formData.confirmPassword}
-                        placeholder="Confirm Password"
-                    />
+                    <div className="input-wrapper">
+                        <input
+                            aria-aria-label='username'
+                            name="username"
+                            type="text"
+                            value={formData.username}
+                            placeholder="Username"
+                        />
+                        <span className="icon-wrapper">
+                            <FontAwesomeIcon icon="user"/>
+                        </span>
+                    </div>
+                    <div className="input-wrapper">
+                        <input
+                            aria-label="email"
+                            name="email"
+                            type="email"
+                            value={formData.email}
+                            placeholder="Email"
+                        />
+                        <span className="icon-wrapper">
+                            <FontAwesomeIcon icon="envelope"/>
+                        </span>
+                    </div>
+                    <div className="input-wrapper">
+                        <input
+                            aria-label='password'
+                            name="password"
+                            type={showPassword ? "text" : "password"}
+                            value={formData.password}
+                            placeholder="Password"
+                        />
+                        <span className="icon-wrapper">
+                                <FontAwesomeIcon icon="lock"/>
+                        </span>
+                    </div>
+                    <div className="input-wrapper">
+                        <input
+                            aria-label='confirm password'
+                            name="confirmPassword"
+                            type={showPassword ? "text" : "password"}
+                            value={formData.confirmPassword}
+                            placeholder="Confirm Password"
+                        />
+                        <span className="icon-wrapper">
+                            <FontAwesomeIcon icon="lock"/>
+                        </span>
+                    </div>
                 </form>
             </div>
             <div className="modal-backdrop"></div>
--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom"
 import store from "./redux";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUser, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faUser, faEnvelope, faLock);
 
 ReactDOM.render(
   <React.StrictMode>
