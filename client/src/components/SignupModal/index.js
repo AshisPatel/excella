341e6731c08e68b99da41565e47f24c164ccddb3
AP <ashis.n.patel@gmail.com>
2021-11-19 21:44:58 -0600

    Make sign up modal more responsive, add close button, login button

--- client/src/components/SignupModal/index.js
+++ client/src/components/SignupModal/index.js
@@ -39,76 +39,97 @@ const SignupModal = ({ setShowSignup }) => {
         <>
             <div className="modal-wrapper">
                 <form className='sl-form'>
-                    <div className="input-wrapper">
-                        <input
-                            aria-aria-label='username'
-                            name="username"
-                            type="text"
-                            className="text-input"
-                            value={formState.username}
-                            onChange={handleChange}
-                            placeholder="Username"
-                        />
-                        <span className="icon-wrapper">
-                            <FontAwesomeIcon icon="user" />
-                        </span>
+                    <span 
+                        className="close-btn" 
+                        aria-label="close"
+                        onClick={() => setShowSignup(false)}    
+                    >
+                            <FontAwesomeIcon icon="window-close"/>
+                    </span>
+                    <h2>
+                       Start <span>excelling</span> with <span>Excella</span>
+                    </h2>
+                    <div className="inputs">
+                        <div className="input-wrapper">
+                            <input
+                                aria-aria-label='username'
+                                name="username"
+                                type="text"
+                                className="text-input"
+                                value={formState.username}
+                                onChange={handleChange}
+                                placeholder="Username"
+                                autoComplete="off"
+                            />
+                            <span className="icon-wrapper">
+                                <FontAwesomeIcon icon="user" />
+                            </span>
+                        </div>
+                        <div className="input-wrapper">
+                            <input
+                                aria-label="email"
+                                name="email"
+                                type="email"
+                                className="text-input"
+                                value={formState.email}
+                                onChange={handleChange}
+                                placeholder="Email"
+                                autoComplete="off"
+                            />
+                            <span className="icon-wrapper">
+                                <FontAwesomeIcon icon="envelope" />
+                            </span>
+                        </div>
+                        <div className="input-wrapper">
+                            <input
+                                aria-label='password'
+                                name="password"
+                                type={showPassword ? "text" : "password"}
+                                className="text-input"
+                                value={formState.password}
+                                onChange={handleChange}
+                                placeholder="Password"
+                            />
+                            <span className="icon-wrapper">
+                                <FontAwesomeIcon icon="lock" />
+                            </span>
+                            <button
+                                className={showPassword ? 'show-btn-clicked' : 'show-btn'}
+                                onClick={toggleShow}
+                                id='show-password'
+                            >
+                                Show
+                            </button>
+                        </div>
+                        <div className="input-wrapper">
+                            <input
+                                aria-label='confirm password'
+                                name="confirmPassword"
+                                type={showConfirmPassword ? "text" : "password"}
+                                className="text-input"
+                                value={formState.confirmPassword}
+                                onChange={handleChange}
+                                placeholder="Confirm Password"
+                            />
+                            <span className="icon-wrapper">
+                                <FontAwesomeIcon icon="lock" />
+                            </span>
+                            <button
+                                className={showConfirmPassword ? 'show-btn-clicked' : 'show-btn'}
+                                onClick={toggleShow}
+                                id='show-confirm-password'
+                            >
+                                Show
+                            </button>
+                        </div>
                     </div>
-                    <div className="input-wrapper">
-                        <input
-                            aria-label="email"
-                            name="email"
-                            type="email"
-                            className="text-input"
-                            value={formState.email}
-                            onChange={handleChange}
-                            placeholder="Email"
-                        />
-                        <span className="icon-wrapper">
-                            <FontAwesomeIcon icon="envelope" />
-                        </span>
-                    </div>
-                    <div className="input-wrapper">
-                        <input
-                            aria-label='password'
-                            name="password"
-                            type={showPassword ? "text" : "password"}
-                            className="text-input"
-                            value={formState.password}
-                            onChange={handleChange}
-                            placeholder="Password"
-                        />
-                        <span className="icon-wrapper">
-                            <FontAwesomeIcon icon="lock" />
-                        </span>
-                        <button
-                            className={showPassword ? 'show-btn-clicked' : 'show-btn'}
-                            onClick={toggleShow}
-                            id='show-password'
-                        >
-                            Show
-                        </button>
-                    </div>
-                    <div className="input-wrapper">
-                        <input
-                            aria-label='confirm password'
-                            name="confirmPassword"
-                            type={showConfirmPassword ? "text" : "password"}
-                            className="text-input"
-                            value={formState.confirmPassword}
-                            onChange={handleChange}
-                            placeholder="Confirm Password"
-                        />
-                        <span className="icon-wrapper">
-                            <FontAwesomeIcon icon="lock" />
-                        </span>
-                        <button
-                            className={showConfirmPassword ? 'show-btn-clicked' : 'show-btn'}
-                            onClick={toggleShow}
-                            id='show-confirm-password'
-                        >
-                            Show
-                        </button>
-                    </div>
+                    <button className="button">
+                        Create Account
+                    </button>
+                    <span className="divider">Already have an account?</span>
+                    <button className="button">
+                        Login
+                    </button>
                 </form>
             </div>
             <div className="modal-backdrop"></div>
--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,9 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom"
 import store from "./redux";
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUser, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faEnvelope, faLock, faWindowClose } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faUser, faEnvelope, faLock);
+library.add(faUser, faEnvelope, faLock, faWindowClose);
 
 ReactDOM.render(
   <React.StrictMode>
