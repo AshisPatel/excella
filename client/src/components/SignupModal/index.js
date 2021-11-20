9b5550b03d827267f3a44c0eb14b4405c0f6d38d
AP <ashis.n.patel@gmail.com>
2021-11-19 20:44:13 -0600

    Add toggle for password display

--- client/src/components/SignupModal/index.js
+++ client/src/components/SignupModal/index.js
@@ -2,14 +2,38 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SignupModal = ({ setShowSignup }) => {
-    const [showPassword, setShowPassword] = useState(false);
     const [warning, setWarning] = useState('');
-    const [formData, setFormData] = useState({
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const [formState, setFormState] = useState({
         username: '',
         email: '',
         password: '',
         confirmPassword: ''
     });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setFormState(prevFormState =>
+            ({
+                ...prevFormState,
+                [name]: value
+            })
+        );
+    }
+
+    const toggleShow = (e) => {
+        e.preventDefault();
+        const id = e.target.id; 
+
+        if (id === 'show-password') {
+            setShowPassword(prevState => !prevState);
+        } else {
+            setShowConfirmPassword(prevState => !prevState);
+        }
+
+    }
 
     return (
         <>
@@ -20,11 +44,13 @@ const SignupModal = ({ setShowSignup }) => {
                             aria-aria-label='username'
                             name="username"
                             type="text"
-                            value={formData.username}
+                            className="text-input"
+                            value={formState.username}
+                            onChange={handleChange}
                             placeholder="Username"
                         />
                         <span className="icon-wrapper">
-                            <FontAwesomeIcon icon="user"/>
+                            <FontAwesomeIcon icon="user" />
                         </span>
                     </div>
                     <div className="input-wrapper">
@@ -32,11 +58,13 @@ const SignupModal = ({ setShowSignup }) => {
                             aria-label="email"
                             name="email"
                             type="email"
-                            value={formData.email}
+                            className="text-input"
+                            value={formState.email}
+                            onChange={handleChange}
                             placeholder="Email"
                         />
                         <span className="icon-wrapper">
-                            <FontAwesomeIcon icon="envelope"/>
+                            <FontAwesomeIcon icon="envelope" />
                         </span>
                     </div>
                     <div className="input-wrapper">
@@ -44,24 +72,42 @@ const SignupModal = ({ setShowSignup }) => {
                             aria-label='password'
                             name="password"
                             type={showPassword ? "text" : "password"}
-                            value={formData.password}
+                            className="text-input"
+                            value={formState.password}
+                            onChange={handleChange}
                             placeholder="Password"
                         />
                         <span className="icon-wrapper">
-                                <FontAwesomeIcon icon="lock"/>
+                            <FontAwesomeIcon icon="lock" />
                         </span>
+                        <button
+                            className={showPassword ? 'show-btn-clicked' : 'show-btn'}
+                            onClick={toggleShow}
+                            id='show-password'
+                        >
+                            Show
+                        </button>
                     </div>
                     <div className="input-wrapper">
                         <input
                             aria-label='confirm password'
                             name="confirmPassword"
-                            type={showPassword ? "text" : "password"}
-                            value={formData.confirmPassword}
+                            type={showConfirmPassword ? "text" : "password"}
+                            className="text-input"
+                            value={formState.confirmPassword}
+                            onChange={handleChange}
                             placeholder="Confirm Password"
                         />
                         <span className="icon-wrapper">
-                            <FontAwesomeIcon icon="lock"/>
+                            <FontAwesomeIcon icon="lock" />
                         </span>
+                        <button
+                            className={showConfirmPassword ? 'show-btn-clicked' : 'show-btn'}
+                            onClick={toggleShow}
+                            id='show-confirm-password'
+                        >
+                            Show
+                        </button>
                     </div>
                 </form>
             </div>
