5dbe0ed81d6bf8f3527595e1a172353b1b4265a5
AP <ashis.n.patel@gmail.com>
2021-11-21 00:15:35 -0600

    Change options icon

--- client/src/components/SideNav/index.js
+++ client/src/components/SideNav/index.js
@@ -52,20 +52,31 @@ const SideNav = () => {
         <>
             {width < transitionWidth &&
                 <button
-                    className={`options side-nav-btn ${display && 'op-selected'}`}
+                    className={`options ${display && 'op-selected'}`}
                     onClick={() => toggleDropDown()}
                 >
                     <FontAwesomeIcon icon="cog" />
-                    <span className="label">
-                        Options
-                    </span>
                 </button>
             }
             {display ?
                 <div className={`side-nav ${clickOff && 'click-off'}`}>
+                    
+                    <Link to="/">
+                        <button
+                            className={`side-nav-btn help-btn ${currentPage === '' && 'sn-selected'} ${width < transitionWidth ? !fadeOut ? 'option-item-in-0' : 'option-item-out-0' : 'option-item-in-0'}`}
+                            name=""
+                            onClick={handleClick}
+                        >
+                            <FontAwesomeIcon icon="home" name="" />
+                            <span className="label">
+                                Home
+                            </span>
+                        </button>
+                    </Link>
+
                     <Link to="/EisenhowerMatrix">
                         <button
-                            className={`side-nav-btn em-btn ${currentPage === 'Eisenhower Matrix' && 'sn-selected'} ${width < transitionWidth ? !fadeOut ? 'option-item-in-0' : 'option-item-out-0' : 'option-item-in-0'}`}
+                            className={`side-nav-btn em-btn ${currentPage === 'Eisenhower Matrix' && 'sn-selected'} ${width < transitionWidth ? !fadeOut ? 'option-item-in-1' : 'option-item-out-1' : 'option-item-in-1'}`}
                             name='Eisenhower Matrix'
                             onClick={handleClick}
                         >
@@ -78,7 +89,7 @@ const SideNav = () => {
 
                     <Link to="/JobCRM">
                         <button
-                            className={`side-nav-btn jc-btn ${currentPage === 'Job CRM' && 'sn-selected'} ${width < transitionWidth ? !fadeOut ? 'option-item-in-1' : 'option-item-out-1' : 'option-item-in-1'}`}
+                            className={`side-nav-btn jc-btn ${currentPage === 'Job CRM' && 'sn-selected'} ${width < transitionWidth ? !fadeOut ? 'option-item-in-2' : 'option-item-out-2' : 'option-item-in-2'}`}
                             name="Job CRM"
                             onClick={handleClick}
                         >
@@ -91,7 +102,7 @@ const SideNav = () => {
 
                     <Link to="/PomodoroTimer">
                         <button
-                            className={`side-nav-btn pt-btn ${currentPage === 'Pomodoro Timer' && 'sn-selected'} ${width < transitionWidth ? !fadeOut ? 'option-item-in-2' : 'option-item-out-2' : 'option-item-in-2'}`}
+                            className={`side-nav-btn pt-btn ${currentPage === 'Pomodoro Timer' && 'sn-selected'} ${width < transitionWidth ? !fadeOut ? 'option-item-in-3' : 'option-item-out-3' : 'option-item-in-3'}`}
                             name='Pomodoro Timer'
                             onClick={handleClick}
                         >
@@ -102,18 +113,6 @@ const SideNav = () => {
                         </button>
                     </Link>
 
-                    <Link to="/">
-                        <button
-                            className={`side-nav-btn help-btn ${currentPage === 'Help' && 'sn-selected'} ${width < transitionWidth ? !fadeOut ? 'option-item-in-3' : 'option-item-out-3' : 'option-item-in-3'}`}
-                            name="Help"
-                            onClick={handleClick}
-                        >
-                            <FontAwesomeIcon icon="question-circle" name="Help" />
-                            <span className="label">
-                                Help
-                            </span>
-                        </button>
-                    </Link>
                 </div>
             :
             <></>}
--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,9 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom"
 import store from "./redux";
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUser, faEnvelope, faLock, faWindowClose, faCheck, faTasks, faQuestionCircle, faClock, faUsers, faCog } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faEnvelope, faLock, faWindowClose, faCheck, faTasks, faQuestionCircle, faClock, faUsers, faCog, faHome } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faUser, faEnvelope, faLock, faWindowClose, faCheck, faTasks, faQuestionCircle, faClock, faUsers, faCog );
+library.add(faUser, faEnvelope, faLock, faWindowClose, faCheck, faTasks, faQuestionCircle, faClock, faUsers, faCog, faHome );
 
 ReactDOM.render(
   <React.StrictMode>
