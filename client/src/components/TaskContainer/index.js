aa23c02c6d497d848474e8a204af8155c418f831
AshisPatel <ashis.n.patel@gmail.com>
2021-11-23 16:43:47 -0600

    Add layout for EisenhowerMatrix and make it responsive -> still need to add task item component

---
+++ client/src/components/TaskContainer/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import './style.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const TaskContainer = (props) => {
+    // create a boolean state variable to see if the object is being hovered -> will allow for conditional rendering of delete all tasks in current category
+    const [hovered, setHovered] = useState(false);
+    const { title, index  } = props;
+    // determine if the box is on the right or left for margin adjustments to keep boxes spaced around the center 
+    const columnClass =  index % 2 === 0 ? 'em-left-box' : 'em-right-box';
+
+    return (
+        <>
+        {/* This column is for extra spacing instead of using bootstraps offset, since offset is implemented as a margin and we need to use a negative margin for the left boxes to move them slightly to the left in order to be centered on large screens */}
+        {index % 2 === 0 && <div className="col-lg-2 spacing-col"></div>}
+        <div
+            className={`offset-1 col-9 offset-lg-0 col-lg-4 ${columnClass} task-container`}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
+            <div className="title-wrapper"> 
+                <h2>{title}</h2>
+                {hovered &&
+                    <button className="clear-btn">
+                        <FontAwesomeIcon icon='trash' />
+                        Clear
+                    </button>
+                }
+            </div>
+
+        </div>
+        </>
+    );
+};
+
+export default TaskContainer;
--- client/src/pages/EisenhowerMatrix/index.js
+++ client/src/pages/EisenhowerMatrix/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import './style.css';
 import { FontAwesomeIcon }from '@fortawesome/react-fontawesome';
+import TaskContainer from "../../components/TaskContainer";
 
 const EisenHowerMatrix = () => {
 
-    const columnSizing = 'offset-1 col-9 offset-md-3 col-md-5 offset-lg-4 col-lg-3'
+    const columnSizing = 'offset-1 col-9 offset-md-3 col-md-5 offset-lg-4 col-lg-4'
+
+    const categories = [
+        {
+            title: 'Do'
+        },
+        {
+            title: 'Do Later'
+        },
+        {
+            title: 'Delegate'
+        },
+        {
+            title: 'Delete'
+        }
+    ]
 
     return (
         <div className="container">
@@ -26,7 +42,9 @@ const EisenHowerMatrix = () => {
                         Delete All
                     </button>
                 </div>
-
+            </div>
+            <div className="row">
+                {categories.map((category, index) => <TaskContainer title={category.title} index={index} key={category.title}/>)}
             </div>
 
         </div>
