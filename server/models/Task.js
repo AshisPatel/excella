fecc80a42b739470013631318547ccb3e78ac9a8
Collin <collinhallett@gmail.com>
2021-11-22 19:29:27 -0600

    updated user and task schema with needed structure

--- server/models/Task.js
+++ server/models/Task.js
@@ -4,7 +4,22 @@ const { Schema } = mongoose;
 
 //creates the taskSchema which we'll import into our User schema
 const taskSchema = new Schema({
-
+    taskContent: {
+        type: String,
+        required: true
+    }, 
+    Category: {
+        type: String,
+        required: true
+    },
+    Complete: {
+        type: Boolean,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 })
 
 module.exports = taskSchema;
--- server/models/User.js
+++ server/models/User.js
@@ -6,7 +6,23 @@ const taskSchema = require('./Task');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-
+    username: {
+        type: String,
+        required: true, 
+        trim: true
+    },
+    email: {
+        type: String, 
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true, 
+        minLength: 5
+    }, 
+    jobs: [jobSchema],
+    tasks: [taskSchema]
 })
 
 const User = mongoose.model('User', userSchema);
