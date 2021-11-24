4beb91cdb8533bb85ae55c68fd33b97a318753d0
AshisPatel <ashis.n.patel@gmail.com>
2021-11-24 16:58:28 -0600

    Add redux slice store and action calls for EisenhowerMatrix / tasks

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,13 +1,16 @@
 import { createStore, combineReducers } from "redux";
 // import reducers to include in root reducer
 import currentPageReducer from "./currentPage";
+import eisenhowerMatrixReducer from "./eisenhowerMatrix";
 // remove once authentication is added
 import loggedInReducer from "./loggedIn";
+
 
 const rootReducer = combineReducers({
     currentPage: currentPageReducer,
     // remove the loggedIn slice when server and auth are implemented
-    loggedIn: loggedInReducer
+    loggedIn: loggedInReducer,
+    tasks: eisenhowerMatrixReducer
 });
 
 
