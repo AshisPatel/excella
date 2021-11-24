7c9e8e077f185512a87f3db92943fac3aa9226c6
AshisPatel <ashis.n.patel@gmail.com>
2021-11-24 12:53:10 -0600

    Style label and text area for tasks

--- client/src/components/TaskModal/index.js
+++ client/src/components/TaskModal/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import validatePassword from '../../utils/validatePassword';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ExcellaIcon from '../ExcellaIcon';
 const TaskModal = (props) => {
     // manipulate state variable to show task modal 
     const { setShowTaskModal } = props;
@@ -13,7 +14,13 @@ const TaskModal = (props) => {
     });
     // warning message on form submission
     const { warning, setWarning } = useState('');
-
+    // array for setting radio buttons
+    const categories = [
+        { name: 'do', label: 'Do' },
+        { name: 'doLater', label: 'Do Later' },
+        { name: 'delegate', label: 'Delegate' },
+        { name: 'delete', label: 'Delete' }
+    ];
     // track change in form inputs
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -59,7 +66,7 @@ const TaskModal = (props) => {
         <>
             <div className="modal-wrapper">
                 <form
-                    className={`modal-form ${fadeOut ? 'slide-out' : 'slide-in'}`}
+                    className={`modal-form task-modal ${fadeOut ? 'slide-out' : 'slide-in'}`}
                     onSubmit={handleSubmit}
                 >
                     <span
@@ -70,51 +77,34 @@ const TaskModal = (props) => {
                         <FontAwesomeIcon icon="window-close" />
                     </span>
 
-                    <label for="content">What do you need to do?</label>
+                    <div className="excella-speech-label">
+                        <ExcellaIcon />
+                        <h2>What do you need to do?</h2>
+                    </div>
                     <textarea
                         name="content"
                         value={formState.content}
                         onChange={handleChange}
+                        placeholder="Enter task here..."
                         rows='2'
                     />
-                    <label for="category">Choose a category:</label>
+                    <div className="excella-speech-label">
+                        <ExcellaIcon />
+                        <h2 className="excella-speech-label">Select a category for this task!</h2>
+                    </div>
                     <div className="radio-btn-wrapper">
-                        <label>
-                            <input
-                                type="radio"
-                                name="category"
-                                value="do"
-                                className="radio-input"
-                                onChange={handleChange}
-                            />Do
-                        </label>
-                        <label>
-                            <input
-                                type="radio"
-                                name="category"
-                                value="doLater"
-                                className="radio-input"
-                                onChange={handleChange}
-                            />Do Later
-                        </label>
-                        <label>
-                            <input
-                                type="radio"
-                                name="category"
-                                value="delegate"
-                                className="radio-input"
-                                onChange={handleChange}
-                            />Delegate
-                        </label>
-                        <label>
-                            <input
-                                type="radio"
-                                name="category"
-                                value="delete"
-                                className="radio-input"
-                                onChange={handleChange}
-                            />Delete
-                        </label>
+
+                        {categories.map(category => (
+                            <label className={formState.category === category.name && 'label-checked'} key={category.name}>
+                                <input
+                                    type="radio"
+                                    name="category"
+                                    value={category.name}
+                                    className="radio-input"
+                                    onChange={handleChange}
+                                />{category.label}
+                            </label>
+                        ))}
                     </div>
                     <button
                         className="button"
