580da82492bdebb6b2534bf9436c903d07f85943
coderjake91 <jacob.b.frazer@gmail.com>
2021-11-25 00:53:37 -0600

    updated graphql schema to include Job model typeDef and initial test resolver for finding all jobs query (.find()), updated models/index.js to export the Job model

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,16 +1,21 @@
-const { User } = require('../models');
+const { User, Job } = require('../models');
+//import custom scalar resolver to format date-time responses in a human-readable format
+const { GraphQLDateTime } = require('graphql-iso-date');
 
 
 const resolvers = {
+    Date: GraphQLDateTime,
     Query: {
       users: async () => {
           return User.find()
       },
       test: async () => {
         return 'This is a test'
+      },
+      jobs: async () => {
+        return Job.find()
       }
     }
   };
-  
 
 module.exports = resolvers
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,15 +1,34 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    scalar Date
     type User {
         _id: ID
         userName: String
         email: String
+        jobs: [Job]
+    }
+
+    type Contact {
+        _id: ID
+        name: String,
+        email: String,
+        phone: String
+    }
+
+    type Job {
+        _id: ID
+        jobTitle: String
+        applicationDate: Date
+        applicationStatus: String
+        username: String
+        contacts: [Contact]
     }
 
     type Query {
         users: [User]
         test: String
+        jobs: [Job]
     }
 `
 
