05b49baeddf83dd070888bba9a5756b1055451e6
coderjake91 <jacob.b.frazer@gmail.com>
2021-11-26 14:35:56 -0600

    updated contactSchema to split the 'name' field into 'firstName' and 'lastName', added a deleteContact resolver, fixed the updateContact resolver to search by _id rather than by contact name, updated the jobSchema to have the date field be 'lastupdated' rather than 'applicationDate', added a query to the resolvers for getting a single Job by _id, all dates are converted to the graphql Date scalar using the npm package graphql-iso-date

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,7 +11,8 @@ const typeDefs = gql`
 
     type Contact {
         _id: ID
-        name: String,
+        firstName: String,
+        lastName: String,
         email: String,
         phone: String
     }
@@ -20,7 +21,7 @@ const typeDefs = gql`
         _id: ID
         jobTitle: String
         employer: String
-        applicationDate: Date
+        lastUpdated: Date
         applicationStatus: String
         username: String
         contacts: [Contact]
@@ -30,14 +31,16 @@ const typeDefs = gql`
         users: [User]
         test: String
         jobs(username: String): [Job]
+        singleJob(_id: ID!): Job
     }
 
     type Mutation {
-        addJob(username: String!, jobTitle: String!, employer: String!, applicationStatus: String!): Job
+        addJob(username: String!, jobTitle: String!, employer: String!, applicationStatus: String!, lastUpdated: String!): Job
         deleteJob(_id: ID!): Job
-        updateJob(_id: ID, jobTitle: String, employer: String, applicationStatus: String): Job
-        addContact(_id: ID!, name: String!, email: String!, phone: String!): Job
-        updateContact(name: String!, email: String!, phone: String!): Contact
+        updateJob(_id: ID!, jobTitle: String, employer: String, applicationStatus: String, lastUpdated: String): Job
+        addContact(_id: ID!, firstName: String!, lastName: String! email: String!, phone: String!): Job
+        deleteContact(_id: ID!): Job
+        updateContact(_id: ID!, firstName: String, lastName: String, email: String, phone: String): Contact
     }
 `
 
